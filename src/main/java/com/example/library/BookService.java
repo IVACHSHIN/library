4411a194487cc8032041ab1f73ba1b00452c39f8
Alexandr <alexandr.ivachshin@gmail.com>
2021-06-09 19:17:47 +0600

    validation with tests

--- src/main/java/com/example/library/BookService.java
+++ src/main/java/com/example/library/BookService.java
@@ -1,17 +1,18 @@
 package com.example.library;
 
+import com.github.dockerjava.api.exception.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.Optional;
 
 @Service
 public class BookService {
 
     @Autowired
-    public BookRepository bookRepository;
+    private BookRepository bookRepository;
 
     public Book get(Long id) {
         Optional<Book> optional = bookRepository.findById(id);
@@ -20,32 +21,31 @@ public class BookService {
             return optional.get();
         }
 
-        throw new RuntimeException("Book not found");
-
+        throw new NotFoundException("Book not found");
     }
 
     public Page<Book> getAll(String query, Pageable pageable) {
         if (query != null) {
-            return bookRepository.findByQuery("%" + query.toLowerCase() + "%", pageable);
-
+            return bookRepository.findByQuery( "%" + query.toLowerCase() + "%", pageable);
         }
         Page<Book> page = bookRepository.findAll(pageable);
         return page;
     }
 
-    public Book create(Book book) {
+    public Book create(BookDto dto) {
+        Book book = new Book(dto);
         Book saved = bookRepository.save(book);
         return saved;
     }
 
-    public Book update(Long id, Book book) {
+    public Book update(Long id, BookDto dto) {
         Book original = get(id);
-        original.setName(book.getName());
-        original.setAuthor(book.getAuthor());
-        original.setDescription(book.getDescription());
-        original.setPublisher(book.getPublisher());
-        original.setIsbn(book.getIsbn());
-        original.setYear(book.getYear());
+        original.setName(dto.getName());
+        original.setAuthor(dto.getAuthor());
+        original.setDescription(dto.getDescription());
+        original.setPublisher(dto.getPublisher());
+        original.setIsbn(dto.getIsbn());
+        original.setYear(dto.getYear());
 
         Book updated = bookRepository.save(original);
         return updated;
